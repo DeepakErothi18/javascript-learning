e4d0341b679f5b1bde429630f78ee33d22a0aace
DeepakErothi18 <145752134+DeepakErothi18@users.noreply.github.com>
2023-09-23 11:44:30 +0000

    variable & datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1820013
+let accountEmail = "deepak18@gmail.com"
+var accountPassword = "1234512345"
+accountCity ="Mumbai"
+
+accountPassword ="123123"
+// accountId = 123321 the constant variable doesn't change the value or update 
+console.log(accountPassword); 
+console.log(accountId);
+accountEmail = "deepu123@gmail.com"
+accountPassword ="786786"
+accountCity = "Benguluru"
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+// data types & object
+
+
+console.log("deepak")
+
+var phoneno = 9121578460
+
+let age = 20 // number 
+
+let a = true // boolen
+
+let b =BigInt("276") + BigInt ("7453")
+
+let username;  // undifine datatype
+
+let s =Symbol("deepak") // symbol datatype
+
+let n =null   // stand alone value
+
+console.log(a,b,n,s,age,phoneno,username);
+
+
+// object 
+
+
+console.log(typeof["deepak"])
